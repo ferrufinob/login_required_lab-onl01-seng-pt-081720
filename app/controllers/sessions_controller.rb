ce8b22bcde6a534066541f50576e0c9cbd0a2fd2
Brenda Ferrufino <ferrufinob@gmail.com>
2021-01-17 00:19:24 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+    render "login"
+  end
+
+  def create
+    if !params[:name] || params[:name].empty?
+      redirect_to "/login"
+    else
+      session[:name] = params[:name]
+    end
+  end
+
+  def destroy
+    session.delete :name
+  end
+end
